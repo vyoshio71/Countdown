1f41002d511184d88d5a7fdb4a554494cfa550ad
Victor Yoshio Tanaka <116774749+vyoshio71@users.noreply.github.com>
2023-04-21 15:49:13 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,29 @@
+const dia = document.getElementById("dia");
+const hora = document.getElementById("hora");
+const minuto = document.getElementById("minuto");
+const segundo = document.getElementById("segundo");
+
+const lancamento = "21 april 2024"
+
+function countDown() {
+    const dataLanc = new Date(lancamento);
+    const hoje = new Date();
+    const segTotal = (dataLanc - hoje) / 1000;
+
+    const finalDias = Math.floor(segTotal / 60 / 60 / 24);
+    const finalHoras = Math.floor(segTotal / 60 / 60) % 24;
+    const finalMinutos = Math.floor(segTotal / 60) % 60;
+    const finalSegundos = Math.floor(segTotal) % 60;
+
+    dia.innerHTML = finalDias
+    hora.innerHTML = formatoTempo(finalHoras)
+    minuto.innerHTML = formatoTempo(finalMinutos)
+    segundo.innerHTML = formatoTempo(finalSegundos)
+}
+
+countDown();
+setInterval(countDown, 1000);
+
+function formatoTempo(tempo) {
+    return tempo < 10? `0${tempo}` : tempo;
+}
